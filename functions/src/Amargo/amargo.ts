6fd2faa7a8750d251a21d32b35e7f6309b0a4087
Lucas <lucasvschenatto@hotmail.com>
2019-11-27 16:54:32 -0300

    removed slack-payload dependency and retesting the quick response

---
+++ functions/src/Amargo/amargo.ts
@@ -0,0 +1,76 @@
+import * as functions from 'firebase-functions'
+import * as httpRequest from 'request'
+import { SlackPayload } from '../localDefinitions'
+import InMemoryAmargo from './inMemory/InMemoryAmargo'
+import FirebaseAmargo from './FirebaseAmargo'
+
+const firebaseAmargo = new FirebaseAmargo()
+const inMemoryAmargo = new InMemoryAmargo()
+
+function amargo(request: functions.https.Request, reply: functions.Response): void {
+    const payload = request.body as SlackPayload
+    const action = payload.text
+    firebaseAmargo.execute(action, payload)
+        .then(response => {
+            const slackResponse = {
+                response_type: "in_channel",
+                text: response
+            }
+            logInputOutput(payload, response)
+            reply.send(slackResponse)
+        })
+        .catch(error => {
+            console.log(error)
+            reply.send(error)
+        })
+}
+function amargoInMemory(request: functions.https.Request, reply: functions.Response): void {
+    reply.write(JSON.stringify({ response_type: "in_channel" }))
+    const payload = request.body as SlackPayload
+    const action = payload.text
+    const response = inMemoryAmargo.execute(action, payload)
+    logInputOutput(payload, response)
+    reply.send(response)
+}
+function amargoBeta(req: functions.https.Request, reply: functions.Response): void {
+    httpRequest.post(req.body.response_url,{body:{"text":"received request","response_type":"ephemeral"}},(error,res,body)=>{
+        if (error) {
+            console.log("-------------error"+JSON.stringify(error))
+        } else{
+            console.log(`statusCode: ${res.statusCode}`)
+            console.log("-----------------body"+JSON.stringify(body))
+        }
+    })
+    // const options: httpRequest.UriOptions & httpRequest.CoreOptions = {
+    //     uri: req.body.response_url,
+    //     headers: {
+    //         "Content-type": "application/json"
+    //     },
+    //     body: {
+    //         "text": "Thanks for your request, we'll process it and get back to you.",
+    //         "response_type": "ephemeral",
+    //     }
+    // }
+    // httpRequest.post(options, (error, res, body) => {
+    //     if (error) {
+    //         console.log(error)
+    //         return
+    //     }
+    //     console.log(`statusCode: ${res.statusCode}`)
+    //     console.log(body)
+    // })
+    amargo(req, reply)
+}
+function ping(request: functions.https.Request, reply: functions.Response): void {
+    reply.send(JSON.stringify(request.body))
+}
+function logInputOutput(payload: SlackPayload, response: string) {
+    console.log("-----------------------------")
+    console.log("Request: " + JSON.stringify(payload))
+    console.log("-----------------------------")
+    console.log("Response: " + response)
+    console.log("-----------------------------")
+}
+
+export default amargo
+export { amargoInMemory, amargoBeta, ping }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,80 +1,9 @@
 import * as functions from 'firebase-functions'
-import * as Payload from "slack-payload"
-import {SlackPayload} from './localDefinitions'
-import InMemoryAmargo from './amargo/inMemory/InMemoryAmargo'
-import FirebaseAmargo from './Amargo/FirebaseAmargo'
-import * as httpRequest from 'request'
+import * as amargo from './amargo/amargo'
 
-const chimas = new InMemoryAmargo()
-const firebaseChimas = new FirebaseAmargo()
+const ping = functions.https.onRequest(amargo.ping)
+const amargoBeta = functions.https.onRequest(amargo.amargoBeta)
+const amargoDefault = functions.https.onRequest(amargo.default)
+const amargoInMemory = functions.https.onRequest(amargo.amargoInMemory)
 
-const amargoInMemory =  (request:functions.https.Request, reply:functions.Response):void => {
-    reply.write(JSON.stringify({response_type: "in_channel"}))
-    const payload =request.body as SlackPayload
-    const action = payload.text
-    const response = chimas.execute(action, payload)
-    logInputOutput(payload,response)
-    reply.send(response)
-}
-
-const amargo =  (request:functions.https.Request, reply:functions.Response):void => {
-    const payload = request.body as SlackPayload
-    const action = payload.text
-    firebaseChimas.execute(action,payload)
-    .then(response=>{
-        const slackResponse = {
-            response_type: "in_channel",
-            text: response
-        }
-        logInputOutput(payload,response)
-        reply.send(slackResponse)
-    })
-    .catch(error=>{
-        console.log(error)
-        reply.send(error)
-    })
-}
-
-const amargoBeta = (req:functions.https.Request, reply:functions.Response):void => {
-    const options = {
-        uri: req.body.response_url,
-        headers: {
-          "Content-type": "application/json"
-        },
-        body:{
-            "text": "Thanks for your request, we'll process it and get back to you.",
-            "response_type": "ephemeral",
-        }
-      }
-    httpRequest.post(options,(error,res,body)=>{
-        if(error){
-            console.log(error)
-            return
-        }
-        console.log(`statusCode: ${res.statusCode}`)
-        console.log(body)
-    })
-    amargo(req,reply)
-}
-
-
-const ping = (request:functions.https.Request, reply:functions.Response):void => {
-    const payload = new Payload(request.body) as SlackPayload
-    reply.send(JSON.stringify(payload))
-}
-
-const httpsPing = functions.https.onRequest(ping)
-const httpsAmargoBeta = functions.https.onRequest(amargoBeta)
-const httpsAmargo = functions.https.onRequest(amargo)
-const httpsAmargoInMemory = functions.https.onRequest(amargoInMemory)
-
-function logInputOutput(payload: SlackPayload, response: string) {
-    console.log("-----------------------------")
-    console.log("Request: "+JSON.stringify(payload))
-    console.log("-----------------------------")
-    console.log("Response: "+response)
-    console.log("-----------------------------")
-}
-
-
-export { httpsAmargo as amargo, httpsAmargoBeta as amargoBeta, httpsAmargoInMemory as amargoInMemory, httpsPing as ping }
+export { amargoDefault as amargo, amargoBeta, amargoInMemory, ping }
